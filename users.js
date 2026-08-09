db19afa9ee2acdbd9e927c16a10534676a7df01e
Kanike Preethi <kanikepreethi98@gmail.com>
2026-08-09 11:03:21 +0530

    Create users.js

---
+++ users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/me', (req, res) => {
+  res.json({ user: req.user || null });
+});
+
+module.exports = router;
